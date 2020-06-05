604009e629f53416c472f934195f07a71786f81f
David Barton <barton.david.sv@gmail.com>
2020-06-05 23:11:01 +0200

    add continueLast feature and began scraping full wiki

--- dataCollectors/dataCollectors.js
+++ dataCollectors/dataCollectors.js
@@ -30,6 +30,11 @@ $ node dataCollectors/dataCollectors.js --noURL
 only the object collectors gonna run, the url-s will be the last ones 
 saved in the directory
 
+'--continueLast' usage:
+$ node dataCollectors/dataCollectors.js --noURL --continueLast
+
+continues the scraping from where it was stopped/script failed
+
 */
 
 const puppeteer = require('puppeteer')
@@ -38,13 +43,14 @@ const duplicateFinder = require('./duplicateFinder')
 
 const wikiaUrlBase = 'https://harrypotter.fandom.com/wiki/'
 const characterUrls = process.argv[2] === '--noURL' ? require('./characterUrls.json') : []
-const charactersObj = []
+const charactersObj = process.argv[3] === '--continueLast' ? require('./characters_TEMP.json') : []
 const errorUrls = []
 
 const dUnderscore = new Date().toLocaleDateString().replace(/\//g, '_')
 const timestamp = (Date.now() / 1000) | 0
 
-let indexId = 1 // used as character ID
+let indexId = process.argv[3] === '--continueLast' ? charactersObj[charactersObj.length - 1].id + 1 : 1 // used as character ID
+let lastCharacterUrl = process.argv[3] === '--continueLast' ? require('./error_log/lastCharacterUrl.json') : 0 // saves where the Object scraping is
 
 async function dataCollectors() {
   const browser = await puppeteer.launch({ headless: true })
@@ -73,7 +79,7 @@ async function dataCollectors() {
   if (process.argv[2] !== '--noURL') {
     for (const category of urlCollectorConfig) {
       try {
-        await urlCollector(category.firstPagePath, category.lastPagePath, category.urlArray, category.jsonName)
+        await urlCollector(category.firstPagePath, category.lastPagePath, eval(category.urlArray), category.jsonName)
       } catch (e) {
         console.error(e)
       }
@@ -81,11 +87,13 @@ async function dataCollectors() {
   }
 
   // collect characters
-  for (const pageUrl of characterUrls) {
+  for (const pageUrl of characterUrls.slice(lastCharacterUrl)) {
     try {
+      lastCharacterUrl++
       await characterCollector(indexId, pageUrl)
       indexId++
       fs.writeFileSync('dataCollectors/characters_TEMP.json', JSON.stringify(charactersObj))
+      fs.writeFileSync('dataCollectors/error_log/lastCharacterUrl.json', JSON.stringify(lastCharacterUrl))
     } catch (e) {
       console.error(e)
       if (e !== '[CHARACTER VALIDATION] failed') {
